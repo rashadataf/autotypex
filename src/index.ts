53edd1a553d8951ad30218281086a1f21eeba08f
Rashad Ataf <rashadataf@gmail.com>
2025-02-20 21:45:27 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { inferType } from './utils/types'
+
+console.log(inferType({ name: "Rashad", age: 30 }));
---
+++ src/utils/types.ts
@@ -0,0 +1,3 @@
+export const inferType = (obj: any): string => {
+    return `This function will infer the type of: ${JSON.stringify(obj)}`;
+}
